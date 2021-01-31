d1f42f50dfc2141d57257ee0d43c7477db7a1785
DanielLG <daniellondono.ing@gmail.com>
2021-01-31 14:39:21 -0500

    audiotest

---
+++ Assets/_Game/Scripts/Audio/RandomAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudio : MonoBehaviour
+{
+    public AudioSource[] audioSource;
+    public AudioClip[] clips;
+    private int indiceAudio;
+
+    public void Play(int i)
+    {
+        indiceAudio = (indiceAudio + 1) % audioSource.Length;
+        audioSource[indiceAudio].clip = clips[i];
+        audioSource[indiceAudio].Play();
+    }
+
+    public void PlayRandom()
+    {
+        Play(Random.Range(0, clips.Length));
+    }
+}
