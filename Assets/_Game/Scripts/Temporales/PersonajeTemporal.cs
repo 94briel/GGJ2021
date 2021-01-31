dbb029f728753ce1ce96588cc0e07ec88b51ca99
Gabriel <gabrielcamargogarcia@gmail.com>
2021-01-31 02:40:40 -0500

    Add movement script

--- Assets/_Game/Scripts/Temporales/PersonajeTemporal.cs
+++ Assets/_Game/Scripts/Temporales/PersonajeTemporal.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class PersonajeTemporal : MonoBehaviour
 {
     public float velocidad = 10;
-
+    public Transform esfera;
+    public Transform arma;
+    public GameObject bala;
     Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-
+    void Update()
+    {
+        Disparar();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         rb.MovePosition(transform.position + (Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal")) * Time.fixedDeltaTime * velocidad);
+        MirarEsfera();
+    }
+
+    public void MirarEsfera()
+    {
+        Vector3 direccion = (esfera.position - transform.position).normalized;
+        Vector3 derecha = Vector3.Cross(Vector3.up, direccion);
+        Vector3 frente = Vector3.Cross(derecha, Vector3.up);
+        transform.forward = frente;
+
+    }
+
+    public void Disparar()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bala, arma.transform.position, arma.transform.rotation);
+        }
     }
 }
