f1a829f740a5f2870412bb19504151bbd98ba050
Morion <adrian.martinez@udea.edu.co>
2021-01-31 21:17:48 -0500

    Pendulo y mejoras

---
+++ Assets/_Game/Scripts/IA/Pendulo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulo : MonoBehaviour
+{
+    
+    void FixedUpdate()
+    {
+        if (EnemigoGrande.singleton != null)
+        {
+            transform.LookAt(EnemigoGrande.singleton.transform);
+        }
+        else
+        {
+            transform.LookAt(Portal.singleton.transform);
+        }
+    }
+}
