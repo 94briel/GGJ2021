01a15360890c2ce653326ebe40a4baab0a892132
Gabriel <gabrielcamargogarcia@gmail.com>
2021-01-31 19:38:09 -0500

    Add Menu

---
+++ Assets/_Game/Scripts/LevelController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public string levelName;
+    public int hasCleared;
+    public UnityEngine.UI.Image key;
+    void Start()
+    {
+        levelName = gameObject.name;
+        key = GetComponent<UnityEngine.UI.Image>();
+        hasCleared = PlayerPrefs.GetInt(levelName,0);
+        key.color = (hasCleared == 1) ? Color.white : key.color;
+    }
+    public void LoadLevel()
+    {
+        PlayerPrefs.SetString("nivel", levelName);
+        SceneManager.LoadScene("Procedural");
+    }
+}
---
+++ Assets/_Game/Scripts/UI/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
